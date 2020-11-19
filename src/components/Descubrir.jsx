6a8557c2281ddcb24ad445ded136cbd627b73038
vivi_soto <anavivi-24@hotmail.com>
2020-11-19 17:56:14 -0500

    cotizacion

---
+++ src/components/Descubrir.jsx
@@ -0,0 +1,28 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import React from 'react';
+import '../assets/styles/components/descubrir.scss';
+
+const Descubrir = () => (
+  <section className='descubrir'>
+    <div className='descubrir__contenedor--form'>
+      <p>Por favor ingresa los siguientes datos para calcular tu descuento. La veracidad de tus datos impacta en el valor de tu descuento.</p>
+      <form>
+        <input name='celular' className='input' type='text' placeholder='Celular' />
+        <input name='correo' className='input' type='text' placeholder='Correo' />
+        <button type='button'>Descubrir Descuento</button>
+      </form>
+      <p className='letra-menuda'>
+        Al continuar aceptas nuestros
+        <a href='/'>Términos y condiciones</a>
+        {' '}
+        &
+        {' '}
+        <a href='/'>Política de Privacidad</a>
+        {' '}
+        para el tratamiento de tus datos.
+      </p>
+    </div>
+  </section>
+);
+
+export default Descubrir;
---
+++ src/components/Figura.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../assets/styles/components/figura.scss';
+
+const Figura = () => (
+  <div className='precio-figura'>
+    <div className='figura'>
+      <p>PRECIO ANTES DEL DESCUENTO</p>
+      <p><b>$$$$$$$$</b></p>
+      <p>Precio establecido por ley*</p>
+    </div>
+  </div>
+);
+
+export default Figura;
