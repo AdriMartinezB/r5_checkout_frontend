af8b14e812f1f4ae457e34db9300c1cc133fc60e
Alejo Avila <alejandroavilaguerrero@gmail.com>
2020-11-22 15:27:13 -0500

    update

--- src/components/IngresaTuPlaca.jsx
+++ src/components/IngresaTuPlaca.jsx
@@ -9,23 +9,18 @@ const IngresaTuPlaca = () => {
   const [placa, setPlaca] = React.useState('');
   const [enable, setEnable] = React.useState(true);
   const regex = /^[A-Za-z]{3}[0-9]{2}[A-Fa-f0-9]{1}$/;
-  function obtenerDataPlaca(placaUp) {
-    fetch(`https://heroprodev.herokuapp.com/api/soatDetails/${placaUp}`, {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      //body: JSON.stringify(data),
-    })
-      .then( res => res.json())
-      .then(data => console.log(data))
-  };
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    const placaUpercase = placa.toUpperCase();
-    console.log(placaUpercase);
-    obtenerDataPlaca(placaUpercase);
+  const changeSubmit = (e) => {
+    setPlaca(e.target.value);
+    const isCorrect = regex.test(e.target.value);
+    if (isCorrect) {
+      const placaUp = e.target.value.toUpperCase();
+        setEnable(false);
+        sessionStorage.setItem('placa',placaUp)
+    } else { 
+      setEnable(true); 
+        sessionStorage.removeItem('placa')
+      }
   };
 
   return (
@@ -37,16 +32,9 @@ const IngresaTuPlaca = () => {
           placeholder='ABC123'
           pattern='[A-Za-z]{3}[0-9]{2}[A-Fa-f0-9]{1}'
           value={placa}
-          onChange={(e) => {
-            setPlaca(e.target.value);
-            const isCorrect = regex.test(e.target.value);
-            if (isCorrect) {
-              setEnable(false);
-            } else { setEnable(true); }
-          }}
+          onChange={changeSubmit}
           required
         />
-        {/* <button className='naranja' disabled={enable}> Cotiza Gratis </button> */}
         <ButtonComponent location='/cotizacion' color='naranja' name='Cotiza Gratis' enable={enable} props={placa}/>
     </section>
   );
--- src/containers/DatosVehiculares.jsx
+++ src/containers/DatosVehiculares.jsx
@@ -5,10 +5,25 @@ import TusDatos from '../components/TusDatos';
 import TuVehiculo from '../components/TuVehiculo';
 
 const DatosVehiculares = () => {
+  const placa = sessionStorage.getItem('placa');
+  const [data, setData] = React.useState({});
+
+  function obtenerData(placaUp) {
+    fetch(`https://heroprodev.herokuapp.com/api/soatDetails/${placaUp}`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      //body: JSON.stringify(data),
+    })
+      .then((res) => res.json())
+      .then((data) => setData(data));
+  };
+  obtenerData(placa);
   return (
     <div className='Vehi--Container'>
-      <TusDatos />
-      <TuVehiculo />
+      <TusDatos nombre={data.OwnerNames} apellido={data.OwnerLastNames} />
+      <TuVehiculo marca={data.CarMake} modelo={data.RegistrationYear} linea={data.CarModel} />
     </div>
   );
 };
