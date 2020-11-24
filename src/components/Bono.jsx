0692e5be3854b71b1a72bb9ec5a188908ae06f69
vivi_soto <anavivi-24@hotmail.com>
2020-11-24 01:16:06 -0500

    componente bono

---
+++ src/components/Bono.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import '../assets/styles/components/bono.scss';
+import moto from '../assets/img/Product/moto.jpg';
+
+const Bono = ({ bono }) => {
+  return (
+    <div className='card'>
+      <p className='card__texto'>{bono}</p>
+      <img className='card__img' src={moto} alt='Imagen Producto' />
+    </div>
+  );
+};
+
+export default Bono;
