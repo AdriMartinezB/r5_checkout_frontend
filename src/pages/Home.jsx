cd348a8bd6271f6a30236371f72a579e40088f53
adrimarb <adpmb1995@hotmail.com>
2020-11-25 17:09:16 -0500

    [ADD] Carrusel

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,10 +3,12 @@ import React from 'react';
 import '../assets/styles/pages/home.scss';
 import InfoSlider from '../components/InfoSlider';
 import Slider from '../components/slider';
+import Carrusel from '../components/Carrusel'
 
 const Home = () => {
   return (
     <div className='home'>
+      <Carrusel />
       <Slider />
       <InfoSlider />
     </div>
