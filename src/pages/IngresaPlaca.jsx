5b34338b2a3808aa742ebd4a89ff0afbbc06d3a6
adrimarb <adpmb1995@hotmail.com>
2020-12-01 20:35:37 -0500

    Siendo utilizadas por el programa

--- src/pages/IngresaPlaca.jsx
+++ src/pages/IngresaPlaca.jsx
@@ -2,11 +2,15 @@ import React from 'react';
 
 import '../assets/styles/pages/ingresaPlaca.scss';
 import Soat from '../containers/Soat';
+import Beneficios from '../containers/PopUpBeneficio';
 
 const IngresaPlaca = () => {
   return (
     <div className='ingresaPlaca'>
-      <Soat />
+      <div className = 'Contenedor_ingresa_placa'>
+        <Soat /> 
+        <Beneficios />
+      </div>
     </div>
   );
 
