e243865a7efc274a9f6b7817685a7f46f3623c9b
Alejo Avila <alejandroavilaguerrero@gmail.com>
2020-12-06 10:00:05 -0500

    loading click  button

--- src/components/IngresaTuPlaca.jsx
+++ src/components/IngresaTuPlaca.jsx
@@ -10,6 +10,7 @@ import '../assets/styles/components/buttomComponent.scss';
 const IngresaTuPlaca = (props) => {
   const [placa, setPlaca] = React.useState('');
   const [enable, setEnable] = React.useState(true);
+  const [loading, setLoading] = React.useState(false);
   const regex = /^[A-Za-z]{3}[0-9]{2}[A-Fa-f0-9]{1}$/;
 
   const changeSubmit = (e) => {
@@ -24,26 +25,34 @@ const IngresaTuPlaca = (props) => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    setLoading(true);
     const placaUp = placa.toUpperCase();
     props.getDataRequest(placaUp, props);
   };
 
   return (
-    <section className='placa'>
-      <h1>Métodos de Pago</h1>
-      <form onSubmit={handleSubmit}>
-        <input
-          type='text'
-          name='Placa'
-          placeholder='ABC123'
-          pattern='[A-Za-z]{3}[0-9]{2}[A-Fa-f0-9]{1}'
-          value={placa}
-          onChange={changeSubmit}
-          required
-        />
-        <button className='naranja' disabled={enable}>Cotiza Gratis</button>
-      </form>
-    </section>
+    loading ? (
+      <section className='placa'>
+        <h1>Loading...</h1>
+      </section>
+    ) :
+      (
+        <section className='placa'>
+          <h1>Métodos de Pago</h1>
+          <form onSubmit={handleSubmit}>
+            <input
+              type='text'
+              name='Placa'
+              placeholder='ABC123'
+              pattern='[A-Za-z]{3}[0-9]{2}[A-Fa-f0-9]{1}'
+              value={placa}
+              onChange={changeSubmit}
+              required
+            />
+            <button className='naranja' disabled={enable}>Cotiza Gratis</button>
+          </form>
+        </section>
+      )
   );
 };
 
