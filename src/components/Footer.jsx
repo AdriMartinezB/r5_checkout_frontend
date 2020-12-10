e9cdcfc47f9feb6daa8be1eee6d07366423f40d5
Jhair Vargas <jhavargashe@gmail.com>
2020-12-10 16:08:21 -0500

    [add] change in footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,37 +8,40 @@ const Footer = () => (
     <Link to='/'>
       <img className='footer__img' src={logoFooter} alt='Logo Grupo R5 footer' />
     </Link>
-    <div className='footer__productos'>
-      <h4>Productos</h4>
-      <p>
-        Comprar SOAT
-        <br />
-        Credito en linea
-        <br />
-        Seguro 4 huellitas
-        <br />
-        Seguro motocicleta
-      </p>
-    </div>
-    <div className='footer__legal'>
-      <h4>Legal</h4>
-      <p>
-        Terminos y condiciones
-        <br />
-        Politica de privacidad
-        <br />
-        Politica de cobranza
-      </p>
-    </div>
-    <div className='footer__contacto'>
-      <h4>Siguenos en:</h4>
-      <p>
-        Facebook
-        <br />
-        Twitter
-        <br />
-        Instagram
-      </p>
+
+    <div className='footer__productos_container'>
+      <div className='footer__productos'>
+        <h4>Productos</h4>
+        <p>
+          Comprar SOAT
+          <br />
+          Credito en linea
+          <br />
+          Seguro 4 huellitas
+          <br />
+          Seguro motocicleta
+        </p>
+      </div>
+      <div className='footer__legal'>
+        <h4>Legal</h4>
+        <p>
+          Terminos y condiciones
+          <br />
+          Politica de privacidad
+          <br />
+          Politica de cobranza
+        </p>
+      </div>
+      <div className='footer__contacto'>
+        <h4>Siguenos en:</h4>
+        <p>
+          Facebook
+          <br />
+          Twitter
+          <br />
+          Instagram
+        </p>
+      </div>
     </div>
   </footer>
 );
